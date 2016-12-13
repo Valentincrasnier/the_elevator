f0d16c559ec394daaca075f03b25e117cee621ca
crasnier <valentin@shopmycourses.com>
2016-12-13 14:29:12 +0100

    Elevator control & moving

--- js/elevator.js
+++ js/elevator.js
@@ -1,40 +1,47 @@
 angular.module("elevator", []).
-controller("ElevatorCtrl", ["$scope", "$interval", function($scope, $interval) {
+controller("ElevatorCtrl", ["$scope", function($scope) {
     // Object representing the car
     var car = $scope.car = {
         active: function(n) {
             return this.floor == n;
         },
         state: function() {
-            var r = this.occupied ? "Occpd " : "Empty ";
-            switch (this.dir) {
-                case -1:
-                    r += "↑↑↑↑";
-                    break;
-                case 1:
-                    r += "↓↓↓↓";
-                    break;
-                case 0:
-                    r += this.open ? "OPEN" : "STOP";
-            }
-            return r;
+            return (this.occupied ? "Occpd " : "Empty ") + (this.open ? "OPEN" : "STOP");
         },
         canOpen: function(n) {
             return this.active(n) && !this.occupied && !this.open;
         },
-        openDoor: function() {
-          this.open = true;
+        openDoor: function(shouldAlert = true) {
+            if (!this.open) {
+                this.open = true;
+            } else if (shouldAlert) {
+                alert("The door is already opened");
+            }
         },
-        closeDoor: function() {
-          this.open = false;
+        closeDoor: function(shouldAlert = true) {
+            if (this.open) {
+                this.open = false;
+            } else if (shouldAlert) {
+                alert("The door is already closed");
+            }
         },
         stepIn: function() {
+            this.openDoor(false);
             this.occupied = true;
+            this.closeDoor();
         },
         stepOut: function() {
+            this.openDoor(false);
             this.occupied = false;
         },
-        dir: 0,
+        call: function(n) {
+            if (!this.occupied) {
+                this.closeDoor(false);
+                this.floor = n;
+            } else {
+                alert("The car is full");
+            }
+        },
         floor: 3,
         open: false,
         occupied: false
@@ -42,33 +49,40 @@ controller("ElevatorCtrl", ["$scope", "$interval", function($scope, $interval) {
 
     // Object representing the control panel in the car
     $scope.panel = {
-        btnClass: function(n) {
-            // This can be used to emulate a LED light near or inside the button
-            // to give feedback to the user.
-            return null;
+        press: function(n) {
+            if ($scope.car.occupied) {
+                $scope.car.closeDoor(false);
+                $scope.car.floor = n;
+            } else {
+                alert("Nobody can press this button");
+            }
         },
-        press: function(n) {},
-        stop: function() {}
     };
 
     // Floors
-    var floors = $scope.floors = [];
-    for (var i = 10; i > 0; i--) floors.push({
-        title: i
-    });
-    floors.push({
-        title: "G"
-    });
+    $scope.howManyFloors = 10;
 
-    // Let's have them know their indices. Zero-indexed, from top to bottom.
-    // Also let's initialize them.
-    floors.forEach(function(floor, n) {
-        floor.n = n;
-        floor.open = false;
-        floor.light = null;
-    });
+    function generateFloors() {
+        var floors = $scope.floors = [];
+        for (var i = Number($scope.howManyFloors); i > 0; i--) floors.push({
+            title: i
+        });
+        floors.push({
+            title: "G"
+        });
 
-    $interval(function() {
-        // TODO: Move the car if necessary
-    }, 1000);
+        floors.forEach(function(floor, n) {
+            floor.n = n;
+            floor.open = false;
+        });
+    }
+    generateFloors();
+
+    $scope.$watch('howManyFloors', () => {
+      if($scope.howManyFloors <= 0) $scope.howManyFloors = 1;
+      generateFloors();
+      if(car.floor > $scope.howManyFloors) {
+        car.floor = 0;
+      }
+    });
 }]);
